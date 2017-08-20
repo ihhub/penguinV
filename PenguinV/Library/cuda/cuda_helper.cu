5ae5369be921ce05b9a6481f6ee94faba1840348
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2017-08-20 23:34:39 +0800

    Add more cuda helper functions for future development

--- PenguinV/Library/cuda/cuda_helper.cu
+++ PenguinV/Library/cuda/cuda_helper.cu
@@ -2,9 +2,9 @@
 #include "cuda_helper.cuh"
 #include "../image_exception.h"
 
-KernelParameters::KernelParameters( uint32_t threadsPerBlock_, uint32_t blocksPerGrid_  )
+KernelParameters::KernelParameters( uint32_t threadsPerBlock_, uint32_t blocksPerGrid_ )
     : threadsPerBlock( threadsPerBlock_ )
-    , blocksPerGrid  ( blocksPerGrid_   )
+    , blocksPerGrid  ( blocksPerGrid_ )
 {
 }
 
@@ -19,4 +19,34 @@ void ValidateLastError()
     cudaError_t error = cudaGetLastError();
     if( error != cudaSuccess )
         throw imageException( "Failed to launch CUDA kernel" );
+}
+
+void cudaCheck( cudaError error )
+{
+    if( error != cudaSuccess ) {
+        cudaGetLastError();
+
+        char errorMessage[64];
+        sprintf( errorMessage, "Failed to run CUDA function with error %d", error );
+
+        throw imageException( errorMessage );
+    }
+}
+
+bool cudaSafeCheck( cudaError error )
+{
+    const bool sucess = (error == cudaSuccess);
+    if( !sucess )
+        cudaGetLastError();
+
+    return sucess;
+}
+
+bool IsCudaSupported()
+{
+    int deviceCount = 0;
+    if( !cudaSafeCheck( cudaGetDeviceCount( &deviceCount ) ) )
+        return false;
+
+    return (deviceCount > 0);
 }
--- PenguinV/Library/cuda/cuda_helper.cuh
+++ PenguinV/Library/cuda/cuda_helper.cuh
@@ -4,7 +4,7 @@
 
 struct KernelParameters
 {
-    KernelParameters( uint32_t threadsPerBlock_, uint32_t blocksPerGrid_  );
+    KernelParameters( uint32_t threadsPerBlock_, uint32_t blocksPerGrid_ );
 
     uint32_t threadsPerBlock;
     uint32_t blocksPerGrid;
@@ -15,3 +15,11 @@ KernelParameters getKernelParameters( uint32_t size );
 
 // Validation of last occured error in functions on host side
 void ValidateLastError();
+
+// Validates cudaError value and throws an except if the value is not cudaSuccess
+void cudaCheck( cudaError error );
+
+// Validates cudaError and returns true if the error is cudaSuccess
+bool cudaSafeCheck( cudaError error );
+
+bool IsCudaSupported(); // returns true if there is any CUDA device in system
