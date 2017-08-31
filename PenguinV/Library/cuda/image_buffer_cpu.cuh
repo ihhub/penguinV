18b18c88d113b44be9fe5578d4fb9005208a6b4d
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2017-08-31 21:03:07 +0800

    Add support of CUDA pinned memory
    close #40

---
+++ PenguinV/Library/cuda/image_buffer_cpu.cuh
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include "../image_buffer.h"
+
+namespace Bitmap_Image_Cuda_Cpu
+{
+    class Image : public Bitmap_Image::Image
+    {
+    public:
+        Image()
+        {
+        }
+
+        Image( uint8_t colorCount_ )
+        {
+            setColorCount( colorCount_ );
+        }
+
+        Image( uint32_t width_, uint32_t height_ )
+        {
+            resize( width_, height_ );
+        }
+
+        Image( uint32_t width_, uint32_t height_, uint8_t colorCount_ )
+        {
+            setColorCount( colorCount_ );
+            resize( width_, height_ );
+        }
+
+        Image( uint32_t width_, uint32_t height_, uint8_t colorCount_, uint8_t alignment_ )
+        {
+            setColorCount( colorCount_ );
+            setAlignment( alignment_ );
+            resize( width_, height_ );
+        }
+
+        Image( const Image & image )
+        {
+            copy( image );
+        }
+
+        Image( Image && image )
+        {
+            swap( image );
+        }
+
+        Image & operator=( const Image & image )
+        {
+            Bitmap_Image::Image::operator=( image );
+
+            return (*this);
+        }
+
+        Image & operator=( Image && image )
+        {
+            swap( image );
+        
+            return (*this);
+        }
+
+        virtual ~Image()
+        {
+            clear();
+        }
+
+        virtual uint8_t * _allocate( size_t size ) const
+        {
+            uint8_t * data = nullptr;
+
+            cudaError_t error = cudaMallocHost( &data, sizeof(uint8_t) * size );
+            if( error != cudaSuccess )
+                throw imageException( "Cannot allocate pinned memory on HOST" );
+
+            return data;
+        }
+
+        virtual void _deallocate( uint8_t * data ) const
+        {
+            cudaError_t error = cudaFreeHost( data );
+            if( error != cudaSuccess )
+                throw imageException( "Cannot deallocate pinned memory on HOST" );
+        }
+    };
+}
---
+++ PenguinV/examples/cuda/pinned_memory/pinned_memory.cpp
@@ -0,0 +1,108 @@
+// Example application of showing of CUDA pinned memory speed advantage
+#include <iostream>
+#include "../../../Library/image_buffer.h"
+#include "../../../Library/cuda/cuda_memory.cuh"
+#include "../../../Library/cuda/image_buffer_cuda.cuh"
+#include "../../../Library/cuda/image_buffer_cpu.cuh"
+#include "../../../Library/cuda/cuda_helper.cuh"
+
+void measureTimingToCuda  ( Bitmap_Image::Image      & in, Bitmap_Image_Cuda::Image & out );
+void measureTimingFromCuda( Bitmap_Image_Cuda::Image & in, Bitmap_Image::Image      & out );
+
+int main()
+{
+    // This example application shows speed difference between normal CPU RAM allocation
+    // and using CUDA pinned memory for copy operations from CUDA device to CPU RAM and
+    /// reverse way
+
+    try // <---- do not forget to put your code into try.. catch block!
+    {
+        // First we check whether any CUDA device is in system
+        if( !Cuda::isCudaSupported() ) {
+            std::cout << "No CUDA devices found in the system" << std::endl;
+            return 0;
+        }
+
+        const uint32_t width = 2048;
+        const uint32_t height = 2048;
+
+        // We allocate image using normal CPU RAM allocation (no magic inside)
+        Bitmap_Image::Image in1( width, height );
+
+        // Then we allocate image using CUDA pinned memory...
+        Bitmap_Image_Cuda_Cpu::Image in2( width, height );
+
+        // Do you see the diffrence in programming syntax? No difference :)
+        // And you can use this image in normal image operations as well
+
+        // Now we allocate image on CUDA device
+        Bitmap_Image_Cuda::Image out( width, height );
+
+        // First we will measure speed to copy image from CPU RAM to CUDA device memory
+        // To avoid caching on CPU and give an advantage for normal CPU allocation
+        // we run functions for CUDA pinned memory image
+        measureTimingToCuda( in2, out );
+        measureTimingToCuda( in1, out );
+
+        measureTimingFromCuda( out, in2 );
+        measureTimingFromCuda( out, in1 );
+    }
+    catch( imageException & ex ) {
+        // uh-oh, something went wrong!
+        std::cout << "Exception " << ex.what() << " raised. Do your black magic to recover..." << std::endl;
+        // your magic code must be here to recover from bad things
+        return 0;
+    }
+    catch( ... ) {
+        // uh-oh, something terrible happen!
+        std::cout << "Something very terrible happen. Do your black magic to recover..." << std::endl;
+        // your magic code must be here to recover from terrible things
+        return 0;
+    }
+
+    std::cout << "Everything went fine." << std::endl;
+
+    return 0;
+}
+
+void measureTimingToCuda( Bitmap_Image::Image & in, Bitmap_Image_Cuda::Image & out )
+{
+    cudaEvent_t start, stop; 
+
+    Cuda::cudaCheck( cudaEventCreate(&start) );
+    Cuda::cudaCheck( cudaEventCreate(&stop) );
+
+    Cuda::cudaCheck( cudaEventRecord(start, 0) );
+
+    const uint32_t size = out.rowSize() * out.height();
+
+    Cuda::cudaCheck( cudaMemcpy(out.data(), in.data(), size, cudaMemcpyHostToDevice) );
+    Cuda::cudaCheck( cudaEventRecord(stop, 0) );
+    Cuda::cudaCheck( cudaEventSynchronize(stop) );
+    
+    float time = 0;
+    Cuda::cudaCheck( cudaEventElapsedTime(&time, start, stop) );
+
+    std::cout << "Host to Device bandwidth (GB/s): " << size * 1e-6 / time << std::endl;
+}
+
+void measureTimingFromCuda( Bitmap_Image_Cuda::Image & in, Bitmap_Image::Image & out )
+{
+    cudaEvent_t start, stop; 
+
+    Cuda::cudaCheck( cudaEventCreate(&start) );
+    Cuda::cudaCheck( cudaEventCreate(&stop) );
+
+    Cuda::cudaCheck( cudaEventRecord(start, 0) );
+
+    const uint32_t size = out.rowSize() * out.height();
+
+    Cuda::cudaCheck( cudaMemcpy(out.data(), in.data(), size, cudaMemcpyDeviceToHost) );
+    Cuda::cudaCheck( cudaEventRecord(stop, 0) );
+    Cuda::cudaCheck( cudaEventSynchronize(stop) );
+    
+    float time = 0;
+    Cuda::cudaCheck( cudaEventElapsedTime(&time, start, stop) );
+
+    std::cout << "Device to Host bandwidth (GB/s): " << size * 1e-6 / time << std::endl;
+}
