8ec0e0c2ba9118c322f6ea1e66915133d0bd7651
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2017-10-05 21:09:22 +0800

    Complete CPU architecture code verification
    close #29

--- PenguinV/Library/penguinv/cpu_id_windows.h
+++ PenguinV/Library/penguinv/cpu_id_windows.h
@@ -8,36 +8,36 @@ struct CpuInformation
     {
         int info[4];
         __cpuidex( info, 0, 0 );
-        int nIds = info[0];
-    
-        if( nIds >= 0x00000001 ) {
+        const int id = info[0];
+
+        if( id >= 0x00000001 ) {
             __cpuidex( info, 0x00000001, 0 );
             return (info[3] & ((int)1 << 26)) != 0;
         }
-    
+
         return false;
     }
-    
+
     static bool isAvxSupported()
     {
         int info[4];
         __cpuidex( info, 0, 0 );
-        int nIds = info[0];
-    
-        if( nIds >= 0x00000007 ) {
+        const int id = info[0];
+
+        if( id >= 0x00000007 ) {
             __cpuidex( info, 0x00000007, 0 );
-            return (info[1] & ((int)1 <<  5)) != 0;
+            return (info[1] & ((int)1 << 5)) != 0;
         }
-    
+
         return false;
     }
-    
+
     static bool isNeonSupported()
     {
-        #ifdef _M_ARM
-            return true;
-        #else
-            return false;
-        #endif
+#ifdef _M_ARM
+        return true;
+#else
+        return false;
+#endif
     }
 };
--- PenguinV/Library/penguinv/cpu_identification.h
+++ PenguinV/Library/penguinv/cpu_identification.h
@@ -7,48 +7,83 @@
 
 #include "cpu_id_windows.h"
 
-	#ifdef _M_ARM
-		#define PENGUINV_NEON_SET
-	#else
-		#define PENGUINV_SSE_SET
+    #ifdef _M_ARM
+        #define PENGUINV_NEON_SET
+    #else
+        #define PENGUINV_SSE_SET
 
-		#ifdef _MSC_VER
-			#if _MSC_VER >= 1700
-				#define PENGUINV_AVX_SET
-			#endif
-		#endif
-	#endif
+        #ifdef _MSC_VER
+            #if _MSC_VER >= 1700
+                #define PENGUINV_AVX_SET
+            #endif
+        #endif
+    #endif
 #elif __APPLE__ // MacOS
 
 #include "cpu_id_macos.h"
 
     #ifdef __arm__
         #define PENGUINV_NEON_SET
-    #else
+    #elif __SSE2__
+        #define PENGUINV_SSE_SET
 
+        #ifdef __AVX2__
+            #define PENGUINV_AVX_SET
+        #endif
     #endif
 
 #elif __linux__ // Linux
 
-    #ifdef __arm__
-        #define PENGUINV_NEON_SET
-    #else
-
-    #endif
-
 #include "cpu_id_linux.h"
 
+    #ifdef __arm__
+        #define PENGUINV_NEON_SET
+    #elif __SSE2__
+        #define PENGUINV_SSE_SET
+    
+        #ifdef __AVX2__
+            #define PENGUINV_AVX_SET
+        #endif
+    #endif
+
 #else
-	#error "Unknown platform"
+    #error "Unknown platform. Is your OS Windows or OSX or Linux?"
+#endif
+
+// We verify SIMD instruction set definition
+#ifdef PENGUINV_NEON_SET
+
+    #ifdef PENGUINV_SSE_SET
+        #error "NEON and SSE cannot be supported on a single CPU. Please check SIMD instruction set verification code"
+    #endif
+    
+    #ifdef PENGUINV_AVX_SET
+        #error "NEON and AVX cannot be supported on a single CPU. Please check SIMD instruction set verification code"
+    #endif
+
+#endif
+
+#ifdef PENGUINV_AVX_SET
+    #ifndef PENGUINV_SSE_SET
+        #error "Non of available processor cannot support only AVX without SSE support. Please check SIMD instruction set verification code"
+    #endif
 #endif
 
 // Identify available technologies during runtime
-#ifdef PENGUINV_AVX_SET
-static const bool isAvxAvailable  = CpuInformation::isAvxSupported();
-#endif
 #ifdef PENGUINV_SSE_SET
 static const bool isSseAvailable  = CpuInformation::isSseSupported();
+#else
+static const bool isSseAvailable  = false;
 #endif
+
+#ifdef PENGUINV_AVX_SET
+static const bool isAvxAvailable  = CpuInformation::isAvxSupported();
+#else
+static const bool isAvxAvailable  = false;
+#endif
+
 #ifdef PENGUINV_NEON_SET
 static const bool isNeonAvailable = CpuInformation::isNeonSupported();
+#else
+static const bool isNeonAvailable = false;
 #endif
