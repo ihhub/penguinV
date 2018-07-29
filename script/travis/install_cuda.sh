f0a2f4a2d3a8bd37d0b99f09f5b92c2899f04c18
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2018-07-29 10:56:12 +0800

    Attempt to fix cuda build

--- script/travis/install_cuda.sh
+++ script/travis/install_cuda.sh
@@ -7,7 +7,7 @@
 # 'source' this script, rather than executing it in a sub-process.
 #
 
-travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo apt-get update -qq
 export CUDA_APT=${CUDA:0:3}
