d80a90a5006e40d963453ca305dd0424104fb67a
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2018-10-21 13:09:47 +0800

    Code cleanup and compilation fixes

--- src/FileOperation/jpeg_file.cpp
+++ src/FileOperation/jpeg_file.cpp
@@ -9,8 +9,26 @@
 #include "jpeg_file.h"
 #include "../image_exception.h"
 
-namespace Jpeg_Ops {
-    PenguinV_Image::Image Load(const std::string & image) {
+namespace
+{
+    struct penguinv_err_mgr {
+        struct jpeg_error_mgr pub;	/* "public" fields */
+        jmp_buf setjmp_buffer;	/* for return to caller */
+    };
+    typedef struct penguinv_err_mgr * err_ptr;
+    
+    void custom_jerr_exit(j_common_ptr info)
+    {
+        err_ptr err = (err_ptr) info->err;
+        (*info->err->output_message) (info);
+        longjmp(err->setjmp_buffer, 1);
+    }
+}
+
+namespace Jpeg_Operation
+{
+    PenguinV_Image::Image Load( const std::string & image )
+    {
         if( path.empty() )
             throw imageException( "Incorrect file path for image file loading" );
 
@@ -24,10 +42,10 @@ namespace Jpeg_Ops {
         /* Allocate and initialize JPEG decompression object */
         cInfo.err = jpeg_std_error(&jErr.pub);
         jErr.pub.error_exit = custom_jerr_exit; // override default error handler
-        if(setjmp(jErr.setjmp_err)){
+        if( setjmp(jErr.setjmp_err) ) {
             jpeg_destroy_decompress(&cInfo);
             fclose(file);
-            return PenguinV_Image::Image()
+            return PenguinV_Image::Image();
         }
 
         jpeg_create_decompress(&cInfo);
@@ -41,20 +59,17 @@ namespace Jpeg_Ops {
         /* perform decompression */
         jpeg_start_decompress(&cInfo);
 
-        const uint32_t width = static_cast<uint32_t>(cInfo.image_width);
-        const uint32_t height = static_cast<uint32_t>(cInfo.image_height);
-        const uint32_t nComponents = static_cast<uint8_t>(cInfo.num_components);
+        const uint32_t width     = static_cast<uint32_t>(cInfo.image_width);
+        const uint32_t height    = static_cast<uint32_t>(cInfo.image_height);
+        const uint8_t colorCount = static_cast<uint8_t >(cInfo.num_components);
 
         // TODO: check color space, currently the template supports grayscale and rgb only
-        PenguinV_Image::Image image(width, height, PenguinV_Image::RGB);
-        uint8_t * dataList = image.data();
-        dataList = new uint8_t[cInfo.image_width*cInfo.image_height*cInfo.num_components]
-
-        while(cInfo.output_scanline < cInfo.image_height)
-        {
-            uint8_t* pix = dataList + cInfo.output_scanline*cInfo.image_width*cInfo.num_components;
-            jpeg_read_scanlines(&cInfo, &pix, 1);
-        }
+        PenguinV_Image::Image image( width, height, colorCount );
+        uint8_t * imageData = image.data();
+        const uint8_t * endData = imageData + image.rowSize() * image.height();
+        
+        for ( ; imageData != endData; imageData += image.rowSize())
+            jpeg_read_scanlines(&cInfo, imageData, 1);
 
         /* finish decompression */
         jpeg_finish_decompress(&cInfo);
@@ -65,11 +80,13 @@ namespace Jpeg_Ops {
         return image;
     }
 
-    void Load(const std::string &path, PenguinV_Image::Image &image) {
+    void Load( const std::string & path, PenguinV_Image::Image & image )
+    {
         image = Load(path);
     }
 
-    void Save(const std::string & path, PenguinV_Image::Image & image) {
+    void Save( const std::string & path, PenguinV_Image::Image & image )
+    {
         if( path.empty() )
             throw imageException( "Incorrect file path for image file saving" );
 
@@ -86,27 +103,20 @@ namespace Jpeg_Ops {
 
         cInfo.image_width = image.width();
         cInfo.image_height = image.height();
-        cInfo.input_components = 3;
+        cInfo.input_components = image.colorCount();
         cInfo.in_color_space = JCS_RGB; // use RGB color space
 
         jpeg_set_defaults(&cInfo);
         jpeg_start_compress(&cInfo, TRUE);
-        uint8_t * dataToWrite = image.data();
 
-        while (cInfo.next_scanline < cInfo.image_height) {
-            uint8_t * dt = dataToWrite[cInfo.next_scanline * cInfo.image_width * 3];
-            jpeg_write_scanlines(&cInfo, dt, 1);
-        }
+        uint8_t * imageData = image.data();
+        const uint8_t * endData = imageData + image.rowSize() * image.height();
+        
+        for ( ; imageData != endData; imageData += image.rowSize())
+            jpeg_write_scanlines(&cInfo, imageData, 1);
 
         jpeg_finish_decompress(&cInfo); /* finish decompression */
         jpeg_destroy_decompress(&cInfo); /* release the JPEG decompression object */
         fclose(file);
-        return;
-    }
-
-    static void custom_jerr_exit(j_common_ptr info) {
-        err_ptr err = (err_ptr) info->err;
-        (*info->err->output_message) (info);
-        longjmp(err->setjmp_buffer, 1);
     }
 }
