44d5dd0da9d2b852d7f4908bbf9f599a487e8aa4
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2018-10-21 13:05:35 +0800

    Code cleanup

--- src/FileOperation/jpeg_file.h
+++ src/FileOperation/jpeg_file.h
@@ -1,6 +1,7 @@
 #include "../image_buffer.h"
 
-namespace Jpeg_Ops {
+namespace Jpeg_Operation
+{
     // load a jpeg image
     PenguinV_Image::Image Load(const std::string & path);
     void Load(const std::string & path, PenguinV_Image::Image & image);
@@ -9,12 +10,4 @@ namespace Jpeg_Ops {
     void Save(const std::string & path, PenguinV_Image::Image & image);
     void Save(const std::string & path, PenguinV_Image::Image & image, uint32_t startX, uint32_t startY,
             uint32_t width, uint32_t height);
-
-    // error helpers
-    struct penguinv_err_mgr {
-        struct jpeg_error_mgr pub;	/* "public" fields */
-        jmp_buf setjmp_buffer;	/* for return to caller */
-    };
-    typedef struct penguinv_err_mgr * err_ptr;
-    static void custom_jerr_exit(j_common_ptr)
 }
