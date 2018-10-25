fd63e399f7a34823176165bb7fe42130ba1873d8
lrm25 <luke.r.mccrone@gmail.com>
2018-10-24 22:24:23 -0600

    Issue #179:  modify unit tests for bitmap operation

--- test/unit_tests/unit_test_bitmap.cpp
+++ test/unit_tests/unit_test_bitmap.cpp
@@ -4,13 +4,13 @@
 
 namespace bitmap_operation
 {
-    bool LoadSaveGrayScaleImage()
+    bool WhiteGrayScaleImage()
     {
         PenguinV_Image::Image original = Unit_Test::whiteImage();
 
-        Bitmap_Operation::Save( "white.bmp", original );
+        Bitmap_Operation::Save( "bitmap.bmp", original );
 
-        PenguinV_Image::Image loaded = Bitmap_Operation::Load( "white.bmp" );
+        PenguinV_Image::Image loaded = Bitmap_Operation::Load( "bitmap.bmp" );
 
         if( original.height() != loaded.height() || original.width() != loaded.width() ||
             original.colorCount() != loaded.colorCount() || !Unit_Test::verifyImage( loaded, 255u ) )
@@ -18,10 +18,54 @@ namespace bitmap_operation
 
         return true;
     }
+
+    bool BlackGrayScaleImage()
+    {
+        PenguinV_Image::Image original = Unit_Test::blackImage();
+        Bitmap_Operation::Save( "bitmap.bmp", original );
+        PenguinV_Image::Image loaded = Bitmap_Operation::Load( "bitmap.bmp" );
+
+        if (original.height() != loaded.height() || 
+            original.width() != loaded.width() ||
+            original.colorCount() != loaded.colorCount() || 
+            !Unit_Test::verifyImage( loaded, 0u ))
+            return false;
+        return true;
+    }
+
+    bool RandomRGBImage()
+    {
+        PenguinV_Image::Image original = Unit_Test::randomImage();
+        Bitmap_Operation::Save("bitmap.bmp", original);
+        PenguinV_Image::Image loaded = Bitmap_Operation::Load("bitmap.bmp");
+
+        if ((original.height() != loaded.height()) ||
+            (original.width() != loaded.width()) ||
+            (original.colorCount() != loaded.colorCount()))
+            return false;
+        else
+        {
+            uint32_t rowsizeDiff = loaded.rowSize() - original.rowSize();
+            for (size_t row = 0; row < original.height(); row++)
+            {
+                if (memcmp(&original.data()[row * original.width()],
+                           &loaded.data()[row * (original.width() + 
+                                                 rowsizeDiff)],
+                           original.width()))
+                    return false;
+            }
+        }
+        return true;
+    }
 }
 
 
 void addTests_Bitmap( UnitTestFramework & framework )
 {
-    ADD_TEST( framework, bitmap_operation::LoadSaveGrayScaleImage );
+    framework.add(bitmap_operation::WhiteGrayScaleImage, 
+                  "Load and save white gray-scale image");
+    framework.add(bitmap_operation::BlackGrayScaleImage, 
+                  "Load and save black gray-scale image");
+    framework.add(bitmap_operation::RandomRGBImage, 
+                  "Load and save random RGB image");
 }
