4c50dfef28f95c47f0290553852bff138e7dc3f4
Yann Labou <Yann.Labou@gmail.com>
2018-10-26 08:37:22 +0200

    I hate batch

--- script/appveyor/find_cuda_opencl.cmd
+++ script/appveyor/find_cuda_opencl.cmd
@@ -1,18 +1,19 @@
 @echo off
-if "%CUDA_PATH%"=="" (
-    echo CUDA_PATH is not defined
-    set CUDA_PATH="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\")
+@setlocal EnableDelayedExpansion
 
-echo %CUDA_PATH%
+if defined CUDA_PATH (
+    set CUDA_PATH ""
+)
+set CUDA_PATH="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\")
+echo set CUDA_PATH=%CUDA_PATH%
 
-for /r %CUDA_PATH% %%a in (*) do (
+for /r "%CUDA_PATH%" %%f in (*) do (
     set "p="
-    if "%%~nxa"=="cl.h" set p=%%~dpnxa
-    if "%%~nxa"=="OpenCL.lib" set p=%%~dpnxa
+    if "%%~nxf"=="cl.h"       set p=%%~dpnxf
+    if "%%~nxf"=="OpenCL.lib" set p=%%~dpnxf
 
     if defined p (
-        echo %p%
-    ) else (
-        echo %p% not found.
+        echo !p!
     )
 )
+endlocal
