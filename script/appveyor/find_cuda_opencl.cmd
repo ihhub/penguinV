690f498bde5b95321f84b159319fd721a1b1df78
Yann Labou <Yann.Labou@gmail.com>
2018-10-26 10:41:08 +0200

    CMake must search OpenCL under CUDA if not found

--- script/appveyor/find_cuda_opencl.cmd
+++ script/appveyor/find_cuda_opencl.cmd
@@ -7,5 +7,5 @@ if defined CUDA_PATH (
 set CUDA_PATH="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\"
 echo set CUDA_PATH=%CUDA_PATH%
 
-dir %CUDA_PATH% /b /s /o:gn
+rem dir %CUDA_PATH% /b /s /o:gn
 endlocal
