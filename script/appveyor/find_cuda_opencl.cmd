d71d63cdbf865f9670bc27c30332e20572610bda
Yann Labou <Yann.Labou@gmail.com>
2018-10-26 09:56:46 +0200

    A simple file listing

--- script/appveyor/find_cuda_opencl.cmd
+++ script/appveyor/find_cuda_opencl.cmd
@@ -7,13 +7,5 @@ if defined CUDA_PATH (
 set CUDA_PATH="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\"
 echo set CUDA_PATH=%CUDA_PATH%
 
-for /r "%CUDA_PATH%" %%f in (*) do (
-    set "p="
-    if "%%~nxf"=="cl.h"       set p=%%~dpnxf
-    if "%%~nxf"=="OpenCL.lib" set p=%%~dpnxf
-
-    if defined p (
-        echo !p!
-    )
-)
+dir %CUDA_PATH% /b /s /o:gn
 endlocal
