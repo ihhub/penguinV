4e4b5816f898cbd859b9ae68a4b0967d7932eba5
Yann Labou <Yann.Labou@gmail.com>
2018-10-26 09:22:40 +0200

    Time waste just because of a typo
    Tests won't run this time since I'm testing

--- script/appveyor/find_cuda_opencl.cmd
+++ script/appveyor/find_cuda_opencl.cmd
@@ -4,7 +4,7 @@
 if defined CUDA_PATH (
     set CUDA_PATH ""
 )
-set CUDA_PATH="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\")
+set CUDA_PATH="%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\"
 echo set CUDA_PATH=%CUDA_PATH%
 
 for /r "%CUDA_PATH%" %%f in (*) do (
