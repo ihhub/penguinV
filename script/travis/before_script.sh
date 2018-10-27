25bd4b73bc1a6df312a523bdf7972b67ae90f544
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2018-10-27 22:08:16 +0800

    Code cleanup

--- script/travis/before_script.sh
+++ script/travis/before_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "${STATIC_ANALYSIS+x}"]; then
+if [[ -z "${STATIC_ANALYSIS+x}"]]; then
 	if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 		source script/travis/install_cuda.sh
 	fi
