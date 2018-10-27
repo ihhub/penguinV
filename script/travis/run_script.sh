d65cbb5d8c33e8e21c6a776e5061871cad350621
Diana Guttman <diana.guttman@gmail.com>
2018-10-27 15:10:44 -0700

    Run cppclean on src, test, examples only.
    - Fixes #196.
    
    Signed-off-by: Diana Guttman <diana.guttman@gmail.com>

--- script/travis/run_script.sh
+++ script/travis/run_script.sh
@@ -3,13 +3,13 @@
 if [ -z "${STATIC_ANALYSIS+x}" ]; then
 	mkdir build && cd build/
 	cmake ../ && cmake --build . && ctest --extra-verbose
-	
+
 	if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 		cd ../test/unit_tests/cuda
 		make
 	fi
 else
 	git clone https://github.com/myint/cppclean
-	./cppclean/cppclean .
+	./cppclean/cppclean src test examples
 	exit 0
 fi
