bdc98e1442007f9722ec61a2fc7e4a76dc6a6869
Yann Labou <Yann.Labou@gmail.com>
2018-10-28 14:18:19 +0100

    Run CUDA unit tests if QT_BASE is not set

--- script/travis/run_script.sh
+++ script/travis/run_script.sh
@@ -4,7 +4,7 @@ if [ -z "${STATIC_ANALYSIS+x}" ]; then
 	mkdir build && cd build/
 	cmake -DPENGUINV_BUILD_QT_EXAMPLE=ON ../ && cmake --build . && ctest --extra-verbose
 
-	if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+	if [[ $TRAVIS_OS_NAME == 'linux' && -z "${QT_BASE}" ]]; then
 		cd ../test/unit_tests/cuda
 		make
 	fi
