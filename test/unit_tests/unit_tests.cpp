99d0e3514cd001c91f588f239e1113fb5f8ff6d9
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2018-11-02 23:20:59 +0800

    Add an input parameter for unit tests
    To set the number of iterations for each tests (by deafult it is 1001).

--- test/unit_tests/unit_tests.cpp
+++ test/unit_tests/unit_tests.cpp
@@ -1,15 +1,22 @@
 // This application is designed to run unit tests on penguinV library
-#include <iostream>
+#include <cstdlib>
 #include "unit_test_bitmap.h"
 #include "unit_test_blob_detection.h"
 #include "unit_test_fft.h"
 #include "unit_test_framework.h"
+#include "unit_test_helper.h"
 #include "unit_test_image_buffer.h"
 #include "unit_test_image_function.h"
 #include "unit_test_math.h"
 
-int main()
+int main( int argc, char * argv[] )
 {
+    if ( argc >= 2 ) {
+        const int testCount = std::atoi( argv[1] );
+        if ( testCount > 0 )
+            Unit_Test::setRunCount( static_cast<uint32_t>( testCount ) );
+    }
+
     // The main purpose of this application is to test everything within library
     // To do this we need an engine (framework) and a bunch of tests
 
