7832d71db0e6b4583e30d8805a021eb4cfdaada6
Yann Labou <Yann.Labou@gmail.com>
2018-11-04 12:26:31 +0100

    Fixing after_success script

--- script/travis/print_valgrind_log.sh
+++ script/travis/print_valgrind_log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$DYNAMIC_ANALYSIS" = "ON" ]; then
+if [[ "${DYNAMIC_ANALYSIS}" == "ON" ]]; then
     cat "${TRAVIS_BUILD_DIR}/build/Testing/Temporary/MemoryChecker.*.log" > out && cat out
     test ! -s out
 fi
