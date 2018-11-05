e0e484c7f41e229c95919edbd83150c3e851fa4d
Yann Labou <yannlabou@users.noreply.github.com>
2018-11-05 16:37:03 +0100

    No need to check for env here

--- script/travis/print_valgrind_log.sh
+++ script/travis/print_valgrind_log.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-if [[ "${DYNAMIC_ANALYSIS}" == "ON" ]]; then
-    cat "${TRAVIS_BUILD_DIR}/build/Testing/Temporary/MemoryChecker.*.log" > out && cat out
-    test ! -s out
-fi
+echo "print_valgrind_log.sh is being executed"
+cat "${TRAVIS_BUILD_DIR}/build/Testing/Temporary/MemoryChecker.*.log" > out && cat out
+test ! -s out
