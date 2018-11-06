4f1c00fee0734ec2c0eb5d9de3b8f6536b10e3ab
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2018-11-06 22:44:56 +0800

    Replace median filter example by common filter example
    To show how each filter works

--- examples/filters/example_filters.cpp
+++ examples/filters/example_filters.cpp
@@ -5,42 +5,37 @@
 #include "../../src/image_function.h"
 #include "../../src/FileOperation/bitmap.h"
 
-void example1();
-void example2();
-
 int main()
 {
-    // This example application is made to show how to work with median filter class
-    // Conditions:
-    // We will generate an image with salt-and-pepper noise
-    // Filter the image and see the results
+    // This example application is made to show how filters work and what which results they produce
 
     try // <---- do not forget to put your code into try.. catch block!
     {
         // Create an uniform image with value 128
-        PenguinV_Image::Image image(1024, 1024);
-        image.fill(128);
+        PenguinV_Image::Image image = Bitmap_Operation::Load( "mercury.bmp" );
 
-        // Generate some salt-and-pepper noise
-        for( uint32_t i = 0; i < 1000; ++i )
-        {
-            uint32_t x = static_cast<uint32_t>(rand()) % image.width();
-            uint32_t y = static_cast<uint32_t>(rand()) % image.height();
+         // If the image is empty it means that the image doesn't exist or the file is not readable
+        if( image.empty() )
+            throw imageException( "Cannot load the image" );
 
-            Image_Function::SetPixel(image, x, y, ( ( i % 2 ) == 0 ) ? 0u : 255u );
-        }
-
-        // Save original image into bitmap
-        Bitmap_Operation::Save( "original.bmp", image );
+        // Convert to gray-scale image if it's not
+        if( image.colorCount() != PenguinV_Image::GRAY_SCALE )
+            image = Image_Function::ConvertToGrayScale( image );
 
         // Create Image object which will contain filtered image
         PenguinV_Image::Image filtered( image.width(), image.height() );
 
-        // Filter it!
+        // Median filtering
         Image_Function::Median( image, filtered, 3 );
+        Bitmap_Operation::Save( "median.bmp", filtered );
 
-        // Save filtered image into bitmap
-        Bitmap_Operation::Save( "filtered.bmp", filtered );
+        // Prewitt filtering
+        Image_Function::Prewitt( image, filtered );
+        Bitmap_Operation::Save( "prewitt.bmp", filtered );
+
+        // Sobel filtering
+        Image_Function::Sobel( image, filtered );
+        Bitmap_Operation::Save( "sobel.bmp", filtered );
     }
     catch( const imageException & ex ) {
         // uh-oh, something went wrong!
