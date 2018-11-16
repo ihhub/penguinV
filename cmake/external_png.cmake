d93c83dd1c169076f95e0d3f6decdd54cecce360
Yann Labou <Yann.Labou@gmail.com>
2018-11-16 09:40:32 +0100

    Back to STATIC imported

--- cmake/external_png.cmake
+++ cmake/external_png.cmake
@@ -58,11 +58,13 @@ foreach(header_file ${PNG_HEADERS})
 endforeach()
 
 ExternalProject_Get_Property(png install_dir)
-add_library(PNG_EXTERNAL INTERFACE IMPORTED GLOBAL)
+add_library(PNG_EXTERNAL STATIC IMPORTED)
 set(PNG_LIBRARY ${PNG_STATIC_LIBRARIES})
 set(PNG_INCLUDE_DIRS ${install_dir}/include)
 # CMake INTERFACE_INCLUDE_DIRECTORIES requires the directory to exists at configure time
 # This is quite unhelpful because those directories are only generated at build time
 file(MAKE_DIRECTORY ${PNG_INCLUDE_DIRS}) # Workaround
-set_property(TARGET PNG_EXTERNAL PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${PNG_INCLUDE_DIRS}")
-set_property(TARGET PNG_EXTERNAL PROPERTY INTERFACE_LINK_LIBRARIES "${PNG_LIBRARY};${ZLIB_STATIC_LIBRARIES}")
+set_target_properties(PNG_EXTERNAL PROPERTIES
+    IMPORTED_LOCATION ${PNG_LIBRARY}
+    INTERFACE_INCLUDE_DIRECTORIES "${PNG_INCLUDE_DIRS}"
+    INTERFACE_LINK_LIBRARIES "${PNG_LIBRARY};${ZLIB_STATIC_LIBRARIES}")
