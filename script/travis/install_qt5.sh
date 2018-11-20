69d6a4f9ae1bc33cb4ad5b7378400187ae3d8ea0
Yann Labou <Yann.Labou@gmail.com>
2018-11-20 06:55:01 +0100

    Helps Unixes finding Qt5

--- script/travis/install_qt5.sh
+++ script/travis/install_qt5.sh
@@ -6,10 +6,11 @@ if [[ $QT_BASE ]]; then
         sudo apt-get update -qq;
         sudo apt-get install -qq qt59base;
         source /opt/qt59/bin/qt59-env.sh;
+        export CMAKE_PREFIX_PATH=/opt/qt59/lib/cmake;
     else
         brew update;
         brew install qt5;
         brew link --force qt5;
-        export CMAKE_PREFIX_PATH=$(brew --prefix qt5);
+        export CMAKE_PREFIX_PATH=$(brew --prefix qt5)/lib/cmake;
     fi
 fi
