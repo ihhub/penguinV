8fcf3751dae037ddc457643b31c1839e1bd47843
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2018-11-24 00:10:05 +0800

    Remove override word

--- src/ui/qt/qt_ui.h
+++ src/ui/qt/qt_ui.h
@@ -10,10 +10,10 @@ public:
     explicit UiWindowQt( const PenguinV_Image::Image & image = PenguinV_Image::Image(), const std::string & title = std::string() );
     virtual ~UiWindowQt();
 
-    virtual void drawPoint( const Point2d & point, const PaintColor & color ) override;
-    virtual void drawLine( const Point2d & start, const Point2d & end, const PaintColor & color ) override;
+    virtual void drawPoint( const Point2d & point, const PaintColor & color );
+    virtual void drawLine( const Point2d & start, const Point2d & end, const PaintColor & color );
 protected:
-    virtual void _display() override;
+    virtual void _display();
 private:
     QPixmap _pixmap;
     QLabel _window;
