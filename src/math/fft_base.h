7b873046ea4907602905d63b06a3b2cdc2e1a985
Matthew McGonagle <mwmcgonagle@gmail.com>
2018-11-28 16:21:09 -0500

    Moved member function definitions of class template BaseComplexData to inside class body.

--- src/math/fft_base.h
+++ src/math/fft_base.h
@@ -17,26 +17,78 @@ namespace FFT
     class BaseComplexData 
     {
     public:
-        BaseComplexData();
+        BaseComplexData()
+            : _data  ( nullptr )
+            , _width (0)
+            , _height(0)
+        {
+        }
 
-        BaseComplexData<DataType> & operator=( const BaseComplexData<DataType> & data );
-        BaseComplexData<DataType> & operator=( BaseComplexData<DataType> && data );
 
-        void resize( uint32_t width_, uint32_t height_ );
+        BaseComplexData<DataType> & operator=( const BaseComplexData<DataType> & data )
+        {
+            _copy( data );
+    
+            return *this;
+        }
 
-        DataType * data(); // returns a pointer to data
-        const DataType * data() const;
-        bool empty() const; // returns true is array is empty (unallocated)
+        BaseComplexData<DataType> & operator=( BaseComplexData<DataType> && data )
+        {
+            _swap( data );
+    
+            return *this;
+        }
+
+        void resize( uint32_t width_, uint32_t height_ )
+        {
+            if( !dimensionsMatch(width_, height_) && width_ != 0 && height_ != 0 ) {
+                _clean();
+    
+                const uint32_t size = width_ * height_;
+    
+                _allocateData(size * sizeof(DataType));
+    
+                _width = width_;
+                _height = height_;
+            }
+        }
+
+        DataType * data() // returns a pointer to data
+        {
+            return _data;
+        }
+
+
+        const DataType * data() const
+        {
+            return _data;
+        }
+
+        bool empty() const // returns true if array is empty (unallocated)
+        {
+            return _data == nullptr;
+        }
+
         uint32_t width() const
         {
             return _width;
         }
+
         uint32_t height() const
         {
             return _height;
         }
-        bool dimensionsMatch( const BaseComplexData<DataType> & other) const;
-        bool dimensionsMatch( uint32_t width, uint32_t height) const;
+
+        bool dimensionsMatch( const BaseComplexData<DataType> & other) const
+        {
+            return dimensionsMatch(other.width(), other.height());
+        }
+
+
+        bool dimensionsMatch( uint32_t width, uint32_t height) const
+        {
+            return _width == width && _height == height;
+        }
 
     protected:
         DataType * _data;
@@ -47,9 +99,35 @@ namespace FFT
         virtual void _freeData() = 0; // Override to deal with actual memory freeing
         virtual void _copyData(const BaseComplexData<DataType> & data) = 0; // Override to deal with actual data copying
 
-        void _clean();
-        void _copy( const BaseComplexData<DataType> & data);
-        void _swap( BaseComplexData<DataType> & data );
+        void _clean()
+        {
+            if( _data != nullptr ) {
+                _freeData();
+                _data = nullptr;
+            }
+    
+            _width = 0;
+            _height = 0;
+        }
+
+
+        void _copy( const BaseComplexData<DataType> & data)
+        {
+            _clean();
+    
+            resize(data._width, data._height);
+    
+            if( !empty() )
+                _copyData(data);
+        }
+
+        void _swap( BaseComplexData<DataType> & data )
+        {
+            std::swap( _data  , data._data );
+            std::swap( _width, data._width);
+            std::swap( _height, data._height); 
+        }
+
     };
 
     // The base class fft execution. Sub-classes need to implement the direct and inverse
@@ -80,104 +158,4 @@ namespace FFT
         virtual void _cleanPlans() = 0;
     }; 
 
-    // Class Template Member Function Definitions 
-    template <typename DataType>
-    BaseComplexData<DataType>::BaseComplexData()
-        : _data  ( nullptr )
-        , _width (0)
-        , _height(0)
-    {
-    }
-
-    template <typename DataType>
-    BaseComplexData<DataType> & BaseComplexData<DataType>::operator=(const BaseComplexData<DataType> & data)
-    {
-        _copy( data );
-
-        return *this;
-    }
-
-    template <typename DataType>
-    BaseComplexData<DataType> & BaseComplexData<DataType>::operator= (BaseComplexData<DataType> && data)
-    {
-        _swap( data );
-
-        return *this;
-    }
-
-    template <typename DataType>
-    void BaseComplexData<DataType>::resize(uint32_t width_, uint32_t height_ )
-    {
-        if( !dimensionsMatch(width_, height_) && width_ != 0 && height_ != 0 ) {
-            _clean();
-
-            const uint32_t size = width_ * height_;
-
-            _allocateData(size * sizeof(DataType));
-
-            _width = width_;
-            _height = height_;
-        }
-    }
-
-    template <typename DataType>
-    DataType * BaseComplexData<DataType>::data()
-    {
-        return _data;
-    }
-
-    template <typename DataType>
-    const DataType * BaseComplexData<DataType>::data() const
-    {
-        return _data;
-    }
-
-    template <typename DataType>
-    bool BaseComplexData<DataType>::empty() const
-    {
-        return _data == nullptr;
-    }
-
-    template <typename DataType>
-    bool BaseComplexData<DataType>::dimensionsMatch( const BaseComplexData<DataType> & other) const 
-    {
-        return dimensionsMatch(other.width(), other.height());
-    }
-
-    template <typename DataType>
-    bool BaseComplexData<DataType>::dimensionsMatch( uint32_t width, uint32_t height) const 
-    {
-        return _width == width && _height == height;
-    }
-
-    template <typename DataType>
-    void BaseComplexData<DataType>::_clean() 
-    {
-        if( _data != nullptr ) {
-            _freeData();
-            _data = nullptr;
-        }
-
-        _width = 0;
-        _height = 0;
-    }
-
-    template <typename DataType>
-    void BaseComplexData<DataType>::_copy( const BaseComplexData<DataType> & data)
-    {
-        _clean();
-
-        resize(data._width, data._height);
-
-        if( !empty() )
-            _copyData(data);
-    }
-    
-    template <typename DataType>
-    void BaseComplexData<DataType>::_swap( BaseComplexData<DataType> & data )
-    {
-        std::swap( _data  , data._data );
-        std::swap( _width, data._width);
-        std::swap( _height, data._height); 
-    }
 }
