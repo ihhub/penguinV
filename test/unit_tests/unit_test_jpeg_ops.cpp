b2aa704f9c68b28ab3702e0f1de7924e17d833d6
ihhub <ihar.hubchyk@gmail.com>
2018-12-04 09:36:30 +0800

    Add proper unit tests

--- test/unit_tests/unit_test_jpeg_ops.cpp
+++ test/unit_tests/unit_test_jpeg_ops.cpp
@@ -1,25 +1,71 @@
+#include <stdio.h>
 #include "unit_test_jpeg_ops.h"
 #include "unit_test_helper.h"
 #include "../../src/FileOperation/jpeg_file.h"
 
 namespace jpeg_operation
 {
+    bool WhiteGrayScaleImage()
+    {
+        const PenguinV_Image::Image original = Unit_Test::whiteImage();
+        Jpeg_Operation::Save( "jpeg.jpg", original );
+
+        const PenguinV_Image::Image loaded = Jpeg_Operation::Load( "jpeg.jpg" );
+        remove("jpeg.jpg");
+
+        if( original.height() != loaded.height() || original.width() != loaded.width() ||
+            original.colorCount() != loaded.colorCount() || !Unit_Test::verifyImage( loaded, 255u ) )
+            return false;
+
+        return true;
+    }
+
+    bool BlackGrayScaleImage()
+    {
+        const PenguinV_Image::Image original = Unit_Test::blackImage();
+        Jpeg_Operation::Save( "jpeg.jpg", original );
+
+        const PenguinV_Image::Image loaded = Jpeg_Operation::Load( "jpeg.jpg" );
+        remove("jpeg.jpg");
+
+        if( original.height() != loaded.height() || original.width() != loaded.width() ||
+            original.colorCount() != loaded.colorCount() || !Unit_Test::verifyImage( loaded, 0u ) )
+            return false;
+
+        return true;
+    }
+
     bool LoadSaveRGBImage()
     {
-        const PenguinV_Image::Image original = Unit_Test::uniformRGBImage();
+        const PenguinV_Image::Image original = Unit_Test::randomRGBImage();
         Jpeg_Operation::Save("rgb.jpg", original);
 
-        const PenguinV_Image::Image loaded = Jpeg_Operation::Load("rgb.bmp");
+        const PenguinV_Image::Image loaded = Jpeg_Operation::Load("jpeg.jpg");
+        remove("jpeg.jpg");
 
-        if (original.height() != loaded.height() || original.width() != loaded.width() ||
-            original.colorCount() != loaded.colorCount() || !Unit_Test::verifyImage(loaded, 255u))
+        if( original.height() != loaded.height() || original.width() != loaded.width() ||
+            original.colorCount() != loaded.colorCount() )
             return false;
-    
+
+        const uint32_t rowSizeIn  = original.rowSize();
+        const uint32_t rowSizeOut = loaded.rowSize();
+        const uint32_t width = original.width() * original.colorCount();
+        const uint8_t * inY  = original.data();
+        const uint8_t * outY = loaded.data();
+        const uint8_t * inYEnd = inY + rowSizeIn * original.height();
+        
+        for ( ; inY != inYEnd; inY += rowSizeIn, outY += rowSizeOut ) {
+            if ( memcmp( inY, outY, width ) != 0 )
+                return false;
+        }
+
         return true;
     }
 }
 
 void addTests_Jpeg( UnitTestFramework & framework )
 {
-    ADD_TEST(framework, jpeg_operation::LoadSaveRGBImage);
+    framework.add(jpeg_operation::WhiteGrayScaleImage, "Load and save white gray-scale image");
+    framework.add(jpeg_operation::BlackGrayScaleImage, "Load and save black gray-scale image");
+    framework.add(jpeg_operation::RandomRGBImage,      "Load and save random RGB image");
 }
