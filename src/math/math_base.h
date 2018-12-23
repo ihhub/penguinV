df5508dd3cb0d653499826f18d25c34457a87f7a
Yann Labou <Yann.Labou@gmail.com>
2018-12-23 12:58:36 +0100

    Initial LineBase2D Implementation
    1. First constructor accepts 2 points of PointBase2d
    2. Second constructor accepts 1 PointBase2d point and angle (in radians)

--- src/math/math_base.h
+++ src/math/math_base.h
@@ -105,5 +105,40 @@ struct PointBase3D : public PointBase2D<_Type>
     _Type z;
 };
 
+template <typename _Type>
+struct LineBase2D
+{
+    LineBase2D(const PointBase2D<_Type> & _p1, const PointBase2D<_Type> & _p2)
+        : p1( _p1 ), p2( _p2 )
+    { }
+
+    LineBase2D(const PointBase2D<_Type> & _p, double angleRadians)
+    {
+        this->p1 = PointBase2D<_Type>(_p.x, _p.y);
+        this->p2 = PointBase2D<_Type>(std::cos(angleRadians) * length(), std::sin(angleRadians) * length());
+
+    }
+
+    bool operator == ( const LineBase2D & line ) const
+    {
+        return p1 == line.p1 && p2 == line.p2;
+    }
+
+    LineBase2D & operator += ( const PointBase2D<_Type> & point )
+    {
+        return LineBase2D(this->p1 + point, this->p2 + point);
+    }
+
+    double length() const
+    {
+        double x = static_cast<double>(p2.x) - static_cast<double>(p1.x);
+        double y = static_cast<double>(p2.y) - static_cast<double>(p1.y);
+        return std::sqrt(x * x + y * y);
+    }
+
+    PointBase2D<_Type> p1;
+    PointBase2D<_Type> p2;
+};
+
 typedef PointBase2D<double> Point2d;
 typedef PointBase3D<double> Point3d;
