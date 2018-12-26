6a3fca13807d1575d0562e5f2dd2d46b47411756
Yann Labou <Yann.Labou@gmail.com>
2018-12-26 22:08:33 +0100

    Fixed invalid commit hash reference

--- script/travis/install_qt5.sh
+++ script/travis/install_qt5.sh
@@ -11,7 +11,7 @@ if [[ $QT_BASE ]]; then
         brew update;
         cd $(brew --repository)/Library/Taps/homebrew/homebrew-core
         git fetch
-        git checkout 3b920b5fed -- Formula/qt.rb # Homebrew qt 5.9.3
+        git checkout 3b920b5 -- Formula/qt.rb # Homebrew qt 5.9.3
         HOMEBREW_NO_AUTO_UPDATE=1 brew install qt5;
         brew link --force qt5;
         export CMAKE_PREFIX_PATH=$(brew --prefix qt5)/lib/cmake;
