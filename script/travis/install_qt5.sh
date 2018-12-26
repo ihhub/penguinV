33264b62fb34d52a0ace46c74117780fddce230c
Yann Labou <Yann.Labou@gmail.com>
2018-12-26 22:37:55 +0100

    git fetch --unshallow to fetch as much as possible
    brew update takes time and is not really necessary on Travis CI

--- script/travis/install_qt5.sh
+++ script/travis/install_qt5.sh
@@ -8,12 +8,13 @@ if [[ $QT_BASE ]]; then
         source /opt/qt59/bin/qt59-env.sh;
         export CMAKE_PREFIX_PATH=/opt/qt59/lib/cmake;
     else
-        brew update;
+        project_dir=$(pwd) # Save current directory
         cd $(brew --repository)/Library/Taps/homebrew/homebrew-core
-        git fetch
+        git fetch --unshallow
         git checkout 3b920b5 -- Formula/qt.rb # Homebrew qt 5.9.3
         HOMEBREW_NO_AUTO_UPDATE=1 brew install qt5;
         brew link --force qt5;
         export CMAKE_PREFIX_PATH=$(brew --prefix qt5)/lib/cmake;
+        cd ${project_dir}
     fi
 fi
