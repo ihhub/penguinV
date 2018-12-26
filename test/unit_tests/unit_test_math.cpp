cfbb7e05cd5e15024b2f7e23cc17b5192d11add3
ihhub <ihar.hubchyk@gmail.com>
2018-12-26 20:58:53 +0800

    Add unit test for line intersection

--- test/unit_tests/unit_test_math.cpp
+++ test/unit_tests/unit_test_math.cpp
@@ -63,6 +63,25 @@ namespace pvmath
         }
         return true;
     }
+    
+    bool lineIntersection()
+    {
+        for( uint32_t i = 0; i < Unit_Test::runCount(); ++i ) {
+            const Point2d point1( Unit_Test::randomValue( -1000, 1000, 0.01 ), Unit_Test::randomValue( -1000, 1000, 0.01 ) );
+            const Point2d point2( Unit_Test::randomValue( -1000, 1000, 0.01 ), Unit_Test::randomValue( -1000, 1000, 0.01 ) );
+            const Line2d line1( point1, point2 );
+            
+            if ( point1 == point2 )
+                continue;
+            
+            const int xCoeff = fabs(point1.x - point2.x) > fabs(point1.y - point2.y) ? -1 : 1;
+            const int yCoeff = xCoeff * -1;
+            const Line2d line2( Point2d( xCoeff * point1.x, yCoeff * point1.y ), Point2d( xCoeff * point2.x, yCoeff * point2.y ) );
+            if ( !line1.isIntersect( line2 ) )
+                return false;
+        }
+        return true;
+    }
 }
 
 void addTests_Math( UnitTestFramework & framework )
@@ -70,4 +89,5 @@ void addTests_Math( UnitTestFramework & framework )
     framework.add(pvmath::houghTransform, "math::Hough Transform");
     framework.add(pvmath::lineConstructor, "math::Line2d constructor");
     framework.add(pvmath::parallelLine, "math::Line2d parallel lines");
+    framework.add(pvmath::lineIntersection, "math::Line2d line intersection");
 }
