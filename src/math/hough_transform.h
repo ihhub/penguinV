6249084e93d6eef7fdf386e3fbfcf29dfa49c49a
Lucas Mongrain <xeaprog@gmail.com>
2018-12-27 01:30:31 -0500

    remove template specialization

--- src/math/hough_transform.h
+++ src/math/hough_transform.h
@@ -8,15 +8,6 @@ namespace
     template <typename _Type>
     bool runHoughTransform( const std::vector< PointBase2D<_Type> > & input, _Type initialAngle, _Type angleTolerance, _Type angleStep,
                                     _Type lineTolerance, std::vector< PointBase2D<_Type> > & outOnLine, std::vector< PointBase2D<_Type> > & outOffLine );
-
-    // All input data for angles are in radians, line tolerance is in user-defined {Point2d} coordinate system units (usually pixels)
-    template<>
-    bool runHoughTransform<double>( const std::vector< PointBase2D<double> > & input, double initialAngle, double angleTolerance, double angleStep,
-                                    double lineTolerance, std::vector< PointBase2D<double> > & outOnLine, std::vector< PointBase2D<double> > & outOffLine );
-    
-    template<>
-    bool runHoughTransform<float>( const std::vector< PointBase2D<float> > & input, float initialAngle, float angleTolerance, float angleStep,
-                                   float lineTolerance, std::vector< PointBase2D<float> > & outOnLine, std::vector< PointBase2D<float> > & outOffLine );
 }
 
 namespace Image_Function
