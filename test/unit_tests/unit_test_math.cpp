1e201432e77792e9273bb294a0df2880f052d011
Lucas Mongrain <xeaprog@gmail.com>
2018-12-27 08:40:35 -0500

    add a typename to the vector in unit_test

--- test/unit_tests/unit_test_math.cpp
+++ test/unit_tests/unit_test_math.cpp
@@ -20,7 +20,7 @@ namespace pvmath
             const _Type sinVal = sin( angle );
             const _Type cosVal = cos( angle );
 
-            for ( std::vector< PointBase2D<_Type> >::iterator p = point.begin(); p != point.end(); ++p ) {
+            for ( typename std::vector< PointBase2D<_Type> >::iterator p = point.begin(); p != point.end(); ++p ) {
                 const _Type x = Unit_Test::randomFloatValue<_Type>( -1000, 1000, 0.01f ) + Unit_Test::randomFloatValue<_Type>( -noiseValue, noiseValue, 0.01f );
                 const _Type y = Unit_Test::randomFloatValue<_Type>( -noiseValue, noiseValue, 0.01 );
 
