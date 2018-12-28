704a5fb384fe9b271dc79d6f993f5dd40030d11e
Yann Labou <Yann.Labou@gmail.com>
2018-12-28 12:11:41 +0100

    Revert png_image.h

--- src/file/png_image.h
+++ src/file/png_image.h
@@ -7,7 +7,7 @@ namespace Png_Operation
     PenguinV_Image::Image Load( const std::string & path );
     void                  Load( const std::string & path, PenguinV_Image::Image & image );
 
-    //void Save( const std::string & path, const PenguinV_Image::Image & image );
+    void Save( const std::string & path, const PenguinV_Image::Image & image );
     void Save( const std::string & path, const PenguinV_Image::Image & image, uint32_t startX, uint32_t startY,
                uint32_t width, uint32_t height );
 }
