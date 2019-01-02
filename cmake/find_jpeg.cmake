474160bbcaf0489ef61be3102a6b8fd39e76e0f7
Yann Labou <Yann.Labou@gmail.com>
2019-01-02 23:02:13 +0100

    Update find_jpeg.cmake

--- cmake/find_jpeg.cmake
+++ cmake/find_jpeg.cmake
@@ -59,6 +59,7 @@ if(PENGUINV_USE_EXTERNAL_JPEG)
         URL https://sourceforge.net/projects/libjpeg-turbo/files/2.0.1/libjpeg-turbo-2.0.1.tar.gz
         URL_MD5 1b05a66aa9b006fd04ed29f408e68f46
         INSTALL_DIR ${JPEG_INSTALL}
+        INSTALL_COMMAND ""
         CMAKE_ARGS
             -DCMAKE_DEBUG_POSTFIX=d
             -DENABLE_SHARED=FALSE
