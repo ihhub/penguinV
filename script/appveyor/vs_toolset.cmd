1146e001e0ac73d3f4e3dbf0d01233a8c51ad968
Yann Labou <Yann.Labou@gmail.com>
2019-01-24 05:47:44 +0100

    Update vs_toolset.cmd

--- script/appveyor/vs_toolset.cmd
+++ script/appveyor/vs_toolset.cmd
@@ -1,7 +1,7 @@
 @echo off
 if "%platform%"=="x86" (
-    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" /x86
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 ) else (
-    call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+    call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" %configuration% /x64
     call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 )
