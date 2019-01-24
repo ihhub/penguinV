3549ec360c7a3a303e30e87b2d2db6bf5b04fa42
Yann Labou <Yann.Labou@gmail.com>
2019-01-24 07:18:22 +0100

    Reactivated other configs

--- script/appveyor/vs_toolset.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-if "%platform%"=="x86" (
-    call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
-) else (
-    call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" %configuration% /x64
-    call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
-)
