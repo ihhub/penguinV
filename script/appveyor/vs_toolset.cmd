8631b75dabd7ff44301a198a2ca7a184d8b2a889
Yann Labou <Yann.Labou@gmail.com>
2019-01-24 06:46:22 +0100

    Fixed path to vcvarsall.bat

--- script/appveyor/vs_toolset.cmd
+++ script/appveyor/vs_toolset.cmd
@@ -1,8 +1,7 @@
 @echo off
-echo %VS90COMNTOOLS%
 if "%platform%"=="x86" (
     call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
 ) else (
     call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" %configuration% /x64
-    call "%VS140COMNTOOLS%\..\..VC\vcvarsall.bat" x86_amd64
+    call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
 )
