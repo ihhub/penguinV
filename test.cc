04abdb9528b571d68411e9234ff9748abdfa9679
Yann Labou <Yann.Labou@gmail.com>
2019-02-12 23:15:34 +0100

    More testing

--- test.cc
+++ test.cc
@@ -1,24 +1,25 @@
 void test()
 {
-        switch (x) {
-        case 0:
-        case 1:
-                do_something();
-                break;
-        case 2:
-                do_something_else();
-                break;
-        default:
-                break;
-        }
-        if (condition)
-                do_something_completely_different();
+    switch (x) {
+    case 0:
+    case 1:
+        do_something(); break;
+    case 2:
+        do_something_else(); break;
+    default: break;
+    }
+    if (condition)
+        do_something_completely_different();
 
-        if (x == y) {
-                q();
-        } else if (x > y) {
-                w();
-        } else {
-                r();
-        }
+    if (x == 
+    y) 
+    {
+        q();
+    }
+    else if (x > y) {
+        w();
+    }
+    else {
+        r();
+    }
 }
