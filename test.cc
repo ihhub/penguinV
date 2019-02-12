e58f1ad51ec0a153abd9be45f1e486e461572994
Yann Labou <Yann.Labou@gmail.com>
2019-02-12 23:01:12 +0100

    Testing on a dummy file

---
+++ test.cc
@@ -0,0 +1,24 @@
+void test()
+{
+        switch (x) {
+        case 0:
+        case 1:
+                do_something();
+                break;
+        case 2:
+                do_something_else();
+                break;
+        default:
+                break;
+        }
+        if (condition)
+                do_something_completely_different();
+
+        if (x == y) {
+                q();
+        } else if (x > y) {
+                w();
+        } else {
+                r();
+        }
+}
