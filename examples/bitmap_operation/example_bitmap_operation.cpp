8560e3d7531d41e527db3caafd0dd09ebdba8080
Yann Labou <Yann.Labou@gmail.com>
2019-02-25 20:46:30 +0100

    Reverted changes of examples/bitmap_operation/example_bitmap_operation.cpp

--- examples/bitmap_operation/example_bitmap_operation.cpp
+++ examples/bitmap_operation/example_bitmap_operation.cpp
@@ -1,8 +1,8 @@
 // Example application of library utilization for bitmaps
-#include "../../src/file/bmp_image.h"
+#include <iostream>
 #include "../../src/image_buffer.h"
 #include "../../src/image_function.h"
-#include <iostream>
+#include "../../src/file/bmp_image.h"
 
 void method1();
 void method2();
@@ -14,8 +14,8 @@ int main()
     // Conditions:
     // - "Houston, we received the image of Mercury!"
     // We have an image of Mercury (24-bit color image). We want to load it,
-    // convert to gray-scale, extract the planet on image by applying thresholding
-    // and save image on storage
+    // convert to gray-scale, extract the planet on image by applying thresholding and
+    // save image on storage
 
     try // <---- do not forget to put your code into try.. catch block!
     {
@@ -24,14 +24,11 @@ int main()
         // Second way to do
         method2();
     }
-    catch ( const std::exception& ex )
-    { // uh-oh, something went wrong!
-        std::cout << "Exception " << ex.what() << " raised. Closing the application..."
-                  << std::endl;
+    catch( const std::exception & ex ) { // uh-oh, something went wrong!
+        std::cout << "Exception " << ex.what() << " raised. Closing the application..." << std::endl;
         return 1;
     }
-    catch ( ... )
-    { // uh-oh, something terrible happen!
+    catch( ... ) { // uh-oh, something terrible happen!
         std::cout << "Generic exception raised. Closing the application..." << std::endl;
         return 2;
     }
@@ -43,23 +40,20 @@ int main()
 void method1()
 {
     // Load an image from storage
-    // Please take note that the image must be in the same folder as this
-    // application or project (for Visual Studio) Otherwise you can change the
-    // path where the image stored
+    // Please take note that the image must be in the same folder as this application or project (for Visual Studio)
+    // Otherwise you can change the path where the image stored
     PenguinV_Image::Image image = Bitmap_Operation::Load( "mercury.bmp" );
 
-    // If the image is empty it means that the image doesn't exist or the file is
-    // not readable
-    if ( image.empty() )
+    // If the image is empty it means that the image doesn't exist or the file is not readable
+    if( image.empty() )
         throw imageException( "Cannot load the image" );
 
     // Convert to gray-scale image if it's not
-    if ( image.colorCount() != PenguinV_Image::GRAY_SCALE )
+    if( image.colorCount() != PenguinV_Image::GRAY_SCALE )
         image = Image_Function::ConvertToGrayScale( image );
 
     // Threshold image with calculated optimal threshold
-    image = Image_Function::Threshold( image, Image_Function::GetThreshold(
-                                                  Image_Function::Histogram( image ) ) );
+    image = Image_Function::Threshold( image, Image_Function::GetThreshold( Image_Function::Histogram( image ) ) );
 
     // Save result
     Bitmap_Operation::Save( "result1.bmp", image );
@@ -68,24 +62,21 @@ void method1()
 void method2()
 {
     // Load an image from storage
-    // Please take note that the image must be in the same folder as this
-    // application or project (for Visual Studio) Otherwise you can change the
-    // path where the image stored
+    // Please take note that the image must be in the same folder as this application or project (for Visual Studio)
+    // Otherwise you can change the path where the image stored
     PenguinV_Image::Image image = Bitmap_Operation::Load( "mercury.bmp" );
 
-    // If the image is empty it means that the image doesn't exist or the file is
-    // not readable
-    if ( image.empty() )
+    // If the image is empty it means that the image doesn't exist or the file is not readable
+    if( image.empty() )
         throw imageException( "Cannot load the image" );
 
     // Convert to gray-scale image if it's not
-    if ( image.colorCount() != PenguinV_Image::GRAY_SCALE )
+    if( image.colorCount() != PenguinV_Image::GRAY_SCALE )
         image = Image_Function::ConvertToGrayScale( image );
 
-    // Threshold image with calculated optimal threshold and directly save result
-    // in file
+    // Threshold image with calculated optimal threshold and directly save result in file
     Bitmap_Operation::Save( "result2.bmp",
-                            image = Image_Function::Threshold( image, Image_Function::GetThreshold(
-                                                                          Image_Function::Histogram(
-                                                                              image ) ) ) );
+                            image = Image_Function::Threshold(
+                                image, Image_Function::GetThreshold(
+                                    Image_Function::Histogram( image ) ) ) );
 }
