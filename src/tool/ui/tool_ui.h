b29e975725505e20d12577b40f60107d9c287b91
ihhub <ihar.hubchyk@gmail.com>
2019-04-12 09:00:50 +0800

    Fix code formatting

--- src/tool/ui/tool_ui.h
+++ src/tool/ui/tool_ui.h
@@ -11,7 +11,7 @@ void DisplayHistogram( UiWindow & window, const std::vector < _Type > & histogra
 
     const _Type minValue = *std::min_element( histogram.cbegin(), histogram.cend() );
     const _Type maxValue = *std::max_element( histogram.cbegin(), histogram.cend() );
-    if ( minValue >= maxValue)
+    if ( minValue >= maxValue )
         return;
 
     const _Type range = maxValue - minValue;
@@ -19,16 +19,16 @@ void DisplayHistogram( UiWindow & window, const std::vector < _Type > & histogra
     PenguinV_Image::Image image( histogram.size(), 100 );
     image.fill( 0u );
 
-	const uint32_t rowSize = image.rowSize();
-	uint8_t * dataX = image.data() + ( image.height() - 1u ) * rowSize;
+    const uint32_t rowSize = image.rowSize();
+    uint8_t * dataX = image.data() + ( image.height() - 1u ) * rowSize;
 
-	for ( size_t i = 0u; i < histogram.size(); ++i, ++dataX ) {
-		const _Type limit = (histogram[i] - minValue) * 100 / range;
-		uint8_t * dataY = dataX;
-		for ( _Type y = 0; y < limit; ++y, dataY -= rowSize )
-			*dataY = 255;
-	}
+    for ( size_t i = 0u; i < histogram.size(); ++i, ++dataX ) {
+        const _Type limit = (histogram[i] - minValue) * 100 / range;
+        uint8_t * dataY = dataX;
+        for ( _Type y = 0; y < limit; ++y, dataY -= rowSize )
+            *dataY = 255;
+    }
 
-	window.setImage( image );
+    window.setImage( image );
     window.show();
 }
