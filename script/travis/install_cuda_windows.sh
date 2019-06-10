bdca74328ec5cfa0ca1480fe70beeae0469c0b89
Yann Labou <Yann.Labou@gmail.com>
2019-06-10 22:45:14 +0200

    Adding Windows CUDA install script

---
+++ script/travis/install_cuda_windows.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu -o pipefail
+
+echo "Downloading CUDA toolkit 8"
+travis_retry wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe
+echo "Installing CUDA toolkit 8"
+./cuda_8.0.44_windows.exe -s compiler_8.0 cublas_8.0 cublas_dev_8.0 cudart_8.0 curand_8.0 curand_dev_8.0
+
+export CUDA_DIR="%ProgramFiles/NVIDIA GPU Computing Toolkit/CUDA/v8.0";
+export PATH=$CUDA_DIR/bin:$CUDA_DIR/libnvvp:$PATH;
+nvcc -V
