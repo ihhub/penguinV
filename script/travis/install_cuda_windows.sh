aa2971ea20723a8fc36b98c1126b1c7d9b68fbbb
Yann Labou <Yann.Labou@gmail.com>
2019-06-10 23:04:00 +0200

    Fixed install script path

--- script/travis/install_cuda_windows.sh
+++ script/travis/install_cuda_windows.sh
@@ -2,10 +2,10 @@
 set -eu -o pipefail
 
 echo "Downloading CUDA toolkit 8"
-travis_retry wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe
+wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe
 echo "Installing CUDA toolkit 8"
 ./cuda_8.0.44_windows.exe -s compiler_8.0 cublas_8.0 cublas_dev_8.0 cudart_8.0 curand_8.0 curand_dev_8.0
 
 export CUDA_DIR="%ProgramFiles/NVIDIA GPU Computing Toolkit/CUDA/v8.0";
-export PATH=$CUDA_DIR/bin:$CUDA_DIR/libnvvp:$PATH;
+export PATH="$CUDA_DIR/bin:$CUDA_DIR/libnvvp:$PATH";
 nvcc -V
