2e1109a28d3bee2cb00f75a2c2874b503748b0a3
Yann Labou <Yann.Labou@gmail.com>
2019-06-12 00:49:23 +0200

    last test for today :)

--- script/travis/install_cuda_windows.sh
+++ script/travis/install_cuda_windows.sh
@@ -4,8 +4,11 @@ echo "Downloading CUDA toolkit 8"
 CUDA_URL='https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe'
 wget -q $CUDA_URL -O $HOME/cuda_8.0.44_windows.exe
 echo "Installing CUDA toolkit 8"
-if [ -f $HOME/cuda_8.0.44_windows.exe ]; then $HOME/cuda_8.0.44_windows.exe --help; fi
-# cmd /C "'$HOME/cuda_8.0.44_windows.exe -s compiler_8.0 cublas_8.0 cublas_dev_8.0 cudart_8.0 curand_8.0 curand_dev_8.0'"
+if [[ -f "$HOME/cuda_8.0.44_windows.exe" ]]; then
+    $HOME/cuda_8.0.44_windows.exe -s compiler_8.0 cublas_8.0 cublas_dev_8.0 cudart_8.0 curand_8.0 curand_dev_8.0
+else 
+    echo "$HOME/cuda_8.0.44_windows.exe does not exist"
+fi
 
 # export CUDA_DIR="%ProgramFiles/NVIDIA GPU Computing Toolkit/CUDA/v8.0";
 # export PATH="$CUDA_DIR/bin:$CUDA_DIR/libnvvp:$PATH";
