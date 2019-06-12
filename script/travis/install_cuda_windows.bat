de78a139c03c482876bf1c866f1b2a75d2b36420
Yann Labou <Yann.Labou@gmail.com>
2019-06-12 22:41:42 +0200

    Some testing

--- script/travis/install_cuda_windows.bat
+++ script/travis/install_cuda_windows.bat
@@ -3,6 +3,7 @@
 echo "Downloading CUDA toolkit 8"
 set CUDA_URL='https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe'
 powershell -Command "Invoke-WebRequest %CUDA_URL% -OutFile cuda_8.0.44_windows.exe"
+certutil -hashfile cuda_8.0.44_windows.exe e7d0b4da6d01c32a6f1997516820fdac
 echo "Installing CUDA toolkit 8"
 if EXIST cuda_8.0.44_windows.exe (
     cuda_8.0.44_windows.exe -s compiler_8.0 cublas_8.0 cublas_dev_8.0 cudart_8.0 curand_8.0 curand_dev_8.0
