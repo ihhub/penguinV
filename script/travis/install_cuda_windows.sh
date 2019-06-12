f357bd93ec5fa17fabea000246f12cee820d318d
Yann Labou <Yann.Labou@gmail.com>
2019-06-12 19:31:28 +0200

    Update install_cuda_windows.sh

--- script/travis/install_cuda_windows.sh
+++ script/travis/install_cuda_windows.sh
@@ -6,7 +6,7 @@ wget -q $CUDA_URL -O $HOME/cuda_8.0.44_windows.exe
 echo "Installing CUDA toolkit 8"
 if [[ -f "$HOME/cuda_8.0.44_windows.exe" ]]; then
     chmod 755 $HOME/cuda_8.0.44_windows.exe
-    ./$HOME/cuda_8.0.44_windows.exe -s compiler_8.0 cublas_8.0 cublas_dev_8.0 cudart_8.0 curand_8.0 curand_dev_8.0
+    . $HOME/cuda_8.0.44_windows.exe -s compiler_8.0 cublas_8.0 cublas_dev_8.0 cudart_8.0 curand_8.0 curand_dev_8.0
 else 
     echo "$HOME/cuda_8.0.44_windows.exe does not exist"
 fi
