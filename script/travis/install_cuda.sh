fcfe55fed2221a652e410b1d32d5110f08cde5d9
Eugene Gagulin <fullset@gmail.com>
2019-09-27 10:32:26 +0300

    Add dev package to installation.

--- script/travis/install_cuda.sh
+++ script/travis/install_cuda.sh
@@ -12,7 +12,7 @@ travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo apt-get update -qq
 export CUDA_APT=${CUDA:0:3}
 export CUDA_APT=${CUDA_APT/./-}
-travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cufft-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}
+travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cufft-${CUDA_APT} cuda-cufft-dev-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}
 travis_retry sudo apt-get clean
 export CUDA_HOME=/usr/local/cuda-${CUDA:0:3}
 export LD_LIBRARY_PATH=${CUDA_HOME}/nvvm/lib64:${LD_LIBRARY_PATH}
