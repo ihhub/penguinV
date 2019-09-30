f4d1280050976222a672a4f58881e4c621784dbb
Eugene Gagulin <fullset@gmail.com>
2019-09-30 12:06:06 +0300

    Add instaling cufft to appveyor.yml

--- script/appveyor/cuda_install.cmd
+++ script/appveyor/cuda_install.cmd
@@ -7,7 +7,9 @@ cuda_8.0.44_windows.exe -s compiler_8.0 ^
                            cublas_dev_8.0 ^
                            cudart_8.0 ^
                            curand_8.0 ^
-                           curand_dev_8.0
+                           curand_dev_8.0 ^
+                           cufft_8.0 ^
+                           cufft_dev_8.0
 
 if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin\cudart64_80.dll" ( 
 echo "Failed to install CUDA"
