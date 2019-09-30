6d6739126f93be31e45dd69e2f5b8f52959c72cc
Rahul <rahul.k.ravishankar@gmail.com>
2019-09-30 23:39:27 +0530

    Updated PenguinV_Image to PenguinV

--- src/image_function_simd.h
+++ src/image_function_simd.h
@@ -4,7 +4,7 @@
 
 namespace Image_Function_Simd
 {
-    using namespace PenguinV_Image;
+    using namespace PenguinV;
 
     Image AbsoluteDifference( const Image & in1, const Image & in2 );
     void  AbsoluteDifference( const Image & in1, const Image & in2, Image & out );
