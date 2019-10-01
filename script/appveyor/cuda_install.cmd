9a621138f52a224235ce43d056b5ad9790fce8f6
Eugene Gagulin <fullset@gmail.com>
2019-10-01 10:01:39 +0300

    Remove installing normal versions of cuda from appveyor.

--- script/appveyor/cuda_install.cmd
+++ script/appveyor/cuda_install.cmd
@@ -3,12 +3,9 @@ echo Downloading CUDA toolkit 8
 appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe -FileName cuda_8.0.44_windows.exe
 echo Installing CUDA toolkit 8
 cuda_8.0.44_windows.exe -s compiler_8.0 ^
-                           cublas_8.0 ^
                            cublas_dev_8.0 ^
                            cudart_8.0 ^
-                           curand_8.0 ^
                            curand_dev_8.0 ^
-                           cufft_8.0 ^
                            cufft_dev_8.0
 
 if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin\cudart64_80.dll" ( 
