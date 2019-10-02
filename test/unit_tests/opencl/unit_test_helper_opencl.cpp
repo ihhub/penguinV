254f2d2a90d070e962359156316870b22ef6c9c1
Joseph Nicholas R. Alcantara <josephnicholasalcantara@gmail.com>
2019-10-02 09:57:59 +0800

    Modify verifyImage function and add offset computation (#553)
    close #544

--- test/unit_tests/opencl/unit_test_helper_opencl.cpp
+++ test/unit_tests/opencl/unit_test_helper_opencl.cpp
@@ -13,25 +13,25 @@ namespace
     const std::string programCode = R"(
         #pragma OPENCL EXTENSION cl_khr_global_int32_base_atomics : enable
 
-        __kernel void isEqualOpenCL( __global const uchar * data, uchar value, uint rowSize, uint width, uint height, volatile __global uint * differenceCount )
+        __kernel void isEqualOpenCL( __global const uchar * data, uint offset, uchar value, uint rowSize, uint width, uint height, volatile __global uint * differenceCount )
         {
             const size_t x = get_global_id(0);
             const size_t y = get_global_id(1);
 
             if( x < width && y < height ) {
-                const size_t id = y * rowSize + x;
+                const size_t id = offset + y * rowSize + x;
                 if( data[id] == value )
                     atomic_add( differenceCount, 1 );
             }
         }
 
-        __kernel void isAnyEqualOpenCL( __global const uchar * data, __global uchar * value, uint valueCount, uint rowSize, uint width, uint height, volatile __global uint * differenceCount )
+        __kernel void isAnyEqualOpenCL( __global const uchar * data, uint offset, __global uchar * value, uint valueCount, uint rowSize, uint width, uint height, volatile __global uint * differenceCount )
         {
             const size_t x = get_global_id(0);
             const size_t y = get_global_id(1);
 
             if( x < width && y < height ) {
-                const size_t id = y * rowSize + x;
+                const size_t id = offset + y * rowSize + x;
 
                 bool equal = false;
 
@@ -68,23 +68,32 @@ namespace Unit_Test
     {
         bool verifyImage( const PenguinV_Image::Image & image, uint8_t value )
         {
+            return verifyImage(image, 0, 0, image.width(), image.height(), value);
+        }
+
+        bool verifyImage( const PenguinV_Image::Image & image, const std::vector < uint8_t > & value )
+        {
+            return verifyImage(image, 0, 0, image.width(), image.height(), value);
+        }
+
+        bool verifyImage( const PenguinV_Image::Image & image, uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint8_t value )
+        {
             multiCL::Type< uint32_t > differenceCount( 0u );
 
             const multiCL::OpenCLProgram & program = GetProgram();
             multiCL::OpenCLKernel kernel( program, "isEqualOpenCL" );
 
             const uint32_t rowSize = image.rowSize();
-            const uint32_t width = image.width() * image.colorCount();
-            const uint32_t height = image.height();
+            width *= image.colorCount();
+            const uint32_t offset = x * rowSize + y;
 
-            kernel.setArgument( image.data(), value, rowSize, width, height, differenceCount.data() );
+            kernel.setArgument( image.data(), offset, value, rowSize, width, height, differenceCount.data() );
 
             multiCL::launchKernel2D( kernel, width, height );
 
             return ( differenceCount.get() == width * height );
         }
-
-        bool verifyImage( const PenguinV_Image::Image & image, const std::vector < uint8_t > & value )
+        bool verifyImage( const PenguinV_Image::Image & image, uint32_t x, uint32_t y, uint32_t width, uint32_t height, const std::vector<uint8_t> & value )
         {
             multiCL::Type< uint32_t > differenceCount( 0u );
 
@@ -95,10 +104,10 @@ namespace Unit_Test
             multiCL::OpenCLKernel kernel( program, "isAnyEqualOpenCL" );
 
             const uint32_t rowSize = image.rowSize();
-            const uint32_t width = image.width() * image.colorCount();
-            const uint32_t height = image.height();
+            width *= image.colorCount();
+            const uint32_t offset = x * rowSize + y;
 
-            kernel.setArgument( image.data(), valueOpenCL, static_cast<uint32_t>( value.size() ), rowSize, width, height, differenceCount.data() );
+            kernel.setArgument( image.data(), offset, valueOpenCL, static_cast<uint32_t>( value.size() ), rowSize, width, height, differenceCount.data() );
 
             multiCL::launchKernel2D( kernel, width, height );
 
