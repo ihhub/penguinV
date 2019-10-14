5f018cdb14892ffc2ec944fc5a405dc3df0161ea
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2019-10-14 10:33:27 +0800

    Code style correction

--- src/penguinv/cpu_id_windows.h
+++ src/penguinv/cpu_id_windows.h
@@ -38,7 +38,7 @@ struct CpuInformation
         __cpuidex( info, 0, 0 );
         const int id = info[0];
 
-        if( id >= 0x00000007 ) {
+        if ( id >= 0x00000007 ) {
             __cpuidex( info, 0x00000007, 0 );
             return (info[1] & ((int)1 << 30)) != 0;
         }
