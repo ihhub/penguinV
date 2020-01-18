5acce0d9c8bd23e58090662331a799c32a4b0230
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2020-01-18 11:58:56 +0800

    Cache zlib for Appveyor

--- cmake/find_png.cmake
+++ cmake/find_png.cmake
@@ -1,6 +1,7 @@
 include(ExternalProject)
 option(PENGUINV_ENABLE_PNG_SUPPORT "Enable support of libpng" ON)
 option(PENGUINV_USE_EXTERNAL_PNG "Download libpng and build from source" OFF)
+option(PENGUINV_INSTALL_ZLIB "Download zlib and build from source" ON)
 
 if(PENGUINV_ENABLE_PNG_SUPPORT)
     find_package(PNG)
@@ -40,37 +41,45 @@ if(PENGUINV_USE_EXTERNAL_PNG)
         set(ZLIB_STATIC_LIBRARIES ${ZLIB_LIB_DIR}/libz.a)
     endif()
 
-    ExternalProject_Add(zlib
-        PREFIX zlib
-        URL https://sourceforge.net/projects/libpng/files/zlib/1.2.11/zlib1211.zip
-        URL_MD5 16b41357b2cd81bca5e1947238e64465
-        BINARY_DIR ${ZLIB_BUILD_DIR}
-        BUILD_BYPRODUCTS ${ZLIB_STATIC_LIBRARIES}
-        DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
-        INSTALL_DIR ${ZLIB_INSTALL}
-        CMAKE_CACHE_ARGS
-            -DCMAKE_BUILD_TYPE:STRING=Release
-            -DCMAKE_INSTALL_PREFIX:STRING=${ZLIB_INSTALL}
-            -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON)
+	if(DEFINED ENV{APPVEYOR})
+        if(EXISTS ${ZLIB_INSTALL})
+            set(PENGUINV_INSTALL_ZLIB OFF CACHE BOOL "" FORCE)
+        endif()
+    endif()
+	
+	if(PENGUINV_INSTALL_ZLIB)
+		ExternalProject_Add(zlib
+			PREFIX zlib
+			URL https://sourceforge.net/projects/libpng/files/zlib/1.2.11/zlib1211.zip
+			URL_MD5 16b41357b2cd81bca5e1947238e64465
+			BINARY_DIR ${ZLIB_BUILD_DIR}
+			BUILD_BYPRODUCTS ${ZLIB_STATIC_LIBRARIES}
+			DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+			INSTALL_DIR ${ZLIB_INSTALL}
+			CMAKE_CACHE_ARGS
+				-DCMAKE_BUILD_TYPE:STRING=Release
+				-DCMAKE_INSTALL_PREFIX:STRING=${ZLIB_INSTALL}
+				-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON)
 
-    add_custom_target(zlib_create_destination_dir
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${ZLIB_INCLUDE_DIR}
-    DEPENDS zlib)
-    add_custom_target(zlib_copy_headers_to_destination 
-    DEPENDS zlib_create_destination_dir)
-    foreach(header_file ${ZLIB_HEADERS})
-    add_custom_command(TARGET zlib_copy_headers_to_destination PRE_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different ${header_file} ${ZLIB_INCLUDE_DIR})
-    endforeach()
-    ExternalProject_Get_Property(zlib install_dir)
-    add_library(ZLIB_EXTERNAL STATIC IMPORTED)
-    set(ZLIB_LIBRARY_RELEASE ${install_dir}/lib/zlibstatic.lib)
-    set(ZLIB_LIBRARY_RELWITHDEBINFO ${install_dir}/lib/zlibstatic.lib)
-    set(ZLIB_LIBRARY_DEBUG ${install_dir}/lib/zlibstaticd.lib)
-    set_target_properties(ZLIB_EXTERNAL PROPERTIES
-        IMPORTED_LOCATION_RELEASE "${ZLIB_LIBRARY_RELEASE}"
-        IMPORTED_LOCATION_RELWITHDEBINFO "${ZLIB_LIBRARY_RELWITHDEBINFO}"
-        IMPORTED_LOCATION_DEBUG "${ZLIB_LIBRARY_DEBUG}")
+		add_custom_target(zlib_create_destination_dir
+		COMMAND ${CMAKE_COMMAND} -E make_directory ${ZLIB_INCLUDE_DIR}
+		DEPENDS zlib)
+		add_custom_target(zlib_copy_headers_to_destination 
+		DEPENDS zlib_create_destination_dir)
+		foreach(header_file ${ZLIB_HEADERS})
+		add_custom_command(TARGET zlib_copy_headers_to_destination PRE_BUILD
+			COMMAND ${CMAKE_COMMAND} -E copy_if_different ${header_file} ${ZLIB_INCLUDE_DIR})
+		endforeach()
+		ExternalProject_Get_Property(zlib install_dir)
+		add_library(ZLIB_EXTERNAL STATIC IMPORTED)
+		set(ZLIB_LIBRARY_RELEASE ${install_dir}/lib/zlibstatic.lib)
+		set(ZLIB_LIBRARY_RELWITHDEBINFO ${install_dir}/lib/zlibstatic.lib)
+		set(ZLIB_LIBRARY_DEBUG ${install_dir}/lib/zlibstaticd.lib)
+		set_target_properties(ZLIB_EXTERNAL PROPERTIES
+			IMPORTED_LOCATION_RELEASE "${ZLIB_LIBRARY_RELEASE}"
+			IMPORTED_LOCATION_RELWITHDEBINFO "${ZLIB_LIBRARY_RELWITHDEBINFO}"
+			IMPORTED_LOCATION_DEBUG "${ZLIB_LIBRARY_DEBUG}")
+	endif()
 
     # PNG
     set(PNG_INSTALL ${CMAKE_BINARY_DIR}/png)
@@ -98,24 +107,44 @@ if(PENGUINV_USE_EXTERNAL_PNG)
         set(PNG_STATIC_LIBRARIES ${PNG_LIB_DIR}/libpng16.a)
     endif()
 
-    ExternalProject_Add(png
-        PREFIX png
-        DEPENDS zlib
-        URL https://sourceforge.net/projects/libpng/files/libpng16/1.6.35/lpng1635.zip
-        URL_MD5 d8bfd42ee9e59404349b50106e04e1f9
-        BINARY_DIR ${PNG_BUILD_DIR}
-        BUILD_BYPRODUCTS ${PNG_STATIC_LIBRARIES}
-        DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
-        INSTALL_DIR ${PNG_INSTALL}
-        CMAKE_CACHE_ARGS
-            -DCMAKE_BUILD_TYPE:STRING=Release
-            -DCMAKE_INSTALL_PREFIX:STRING=${PNG_INSTALL}
-            -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
-            -DPNG_TESTS:BOOL=OFF
-            -DPNG_SHARED:BOOL=OFF
-            -DPNG_STATIC:BOOL=ON
-            -DSKIP_INSTALL_EXECUTABLES:BOOL=OFF
-        -DZLIB_ROOT:STRING=${ZLIB_INSTALL})
+	if(PENGUINV_INSTALL_ZLIB)
+		ExternalProject_Add(png
+			PREFIX png
+			DEPENDS zlib
+			URL https://sourceforge.net/projects/libpng/files/libpng16/1.6.35/lpng1635.zip
+			URL_MD5 d8bfd42ee9e59404349b50106e04e1f9
+			BINARY_DIR ${PNG_BUILD_DIR}
+			BUILD_BYPRODUCTS ${PNG_STATIC_LIBRARIES}
+			DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+			INSTALL_DIR ${PNG_INSTALL}
+			CMAKE_CACHE_ARGS
+				-DCMAKE_BUILD_TYPE:STRING=Release
+				-DCMAKE_INSTALL_PREFIX:STRING=${PNG_INSTALL}
+				-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
+				-DPNG_TESTS:BOOL=OFF
+				-DPNG_SHARED:BOOL=OFF
+				-DPNG_STATIC:BOOL=ON
+				-DSKIP_INSTALL_EXECUTABLES:BOOL=OFF
+			-DZLIB_ROOT:STRING=${ZLIB_INSTALL})
+	else()
+		ExternalProject_Add(png
+			PREFIX png
+			URL https://sourceforge.net/projects/libpng/files/libpng16/1.6.35/lpng1635.zip
+			URL_MD5 d8bfd42ee9e59404349b50106e04e1f9
+			BINARY_DIR ${PNG_BUILD_DIR}
+			BUILD_BYPRODUCTS ${PNG_STATIC_LIBRARIES}
+			DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+			INSTALL_DIR ${PNG_INSTALL}
+			CMAKE_CACHE_ARGS
+				-DCMAKE_BUILD_TYPE:STRING=Release
+				-DCMAKE_INSTALL_PREFIX:STRING=${PNG_INSTALL}
+				-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
+				-DPNG_TESTS:BOOL=OFF
+				-DPNG_SHARED:BOOL=OFF
+				-DPNG_STATIC:BOOL=ON
+				-DSKIP_INSTALL_EXECUTABLES:BOOL=OFF
+			-DZLIB_ROOT:STRING=${ZLIB_INSTALL})
+	endif()
 
     add_custom_target(png_create_destination_dir
         COMMAND ${CMAKE_COMMAND} -E make_directory ${PNG_INCLUDE_DIR}
