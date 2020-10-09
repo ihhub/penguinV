b4813df3c2c222cf999930efa5314b4073b735ea
Krzysztof Gorecki <kg@b-intohimo.com>
2020-10-09 15:07:54 +0300

    removed cmake garbage

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kgorecki/projects/penguinV
-# Build directory: /Users/kgorecki/projects/penguinV
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("examples")
