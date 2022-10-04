eb616a7d3fd81a83b9da8f4fa7731cb8dd2cdfeb
Ihar Hubchyk <ihar.hubchyk@gmail.com>
2022-10-04 23:16:14 +0800

    Update example_blob_detection.cpp

--- examples/blob_detection/example_blob_detection.cpp
+++ examples/blob_detection/example_blob_detection.cpp
@@ -10,7 +10,7 @@ int main( int argc, char * argv[] )
     std::string filePath = "mercury.bmp"; // default image path
     if ( argc > 1 ) { // Check input data
         filePath = argv[1];
-	}
+    }
 
     try // <---- do not forget to put your code into try.. catch block!
     {
