21c2706055dcb3b128a23f658719df9923efdc6f
Matthew Henderson <32535159+matthewroberthenderson@users.noreply.github.com>
2023-11-01 20:28:50 +1100

    change files to have relative path for example resources

--- examples/filters/example_filters.cpp
+++ examples/filters/example_filters.cpp
@@ -31,7 +31,7 @@ int main( int argc, char * argv[] )
 
     try // <---- do not forget to put your code into try.. catch block!
     {
-        std::string filePath = "lena.bmp"; // default image path
+        std::string filePath = "../../data/lena.bmp"; // default image path
         if ( argc > 1 ) // Check input data
             filePath = argv[1];
 
